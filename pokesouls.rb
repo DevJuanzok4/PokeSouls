663d73ca26f73d593a5a191ea70fd204ce1ecedd
Juan C <121655192+DevJuanzok4@users.noreply.github.com>
2023-08-14 01:59:29 +0000

    chore: q q eu to fazendo da minha vida

--- pokesouls.rb
+++ pokesouls.rb
@@ -1,6 +1,5 @@
 #Fonte Grande para titulos
 titulo = "\e[30;48;5;235m\e[2J\e[H\e[3J\e[8;40;160t\e[5;90;140t\e[1;130t\e[23;58;170t\e[28;38;175t\e[17;50;200t\e[16;100;200t\e[21;100;220t\e[23;110;240t\e[24;150;250t\e[25;170;250t\e[26;200;255t\e[28;230;255t\e[30;255;255t\e[31;255;240t\e[32;255;230t\e[33;255;215t\e[34;255;200t\e[35;255;180t\e[36;255;160t\e[37;255;130t\e[38;255;100t\e[39;255;70t\e[40;255;40t\e[41;255;10t\e[42;240;0t\e[43;220;0t\e[44;200;0t\e[45;180;0t\e[46;160;0t\e[47;140;0t\e[48;120;0t\e[49;100;0t\e[0m"
-
 # Classe Jogador
 class Jogador
   attr_reader :nome, :time_jogador
@@ -92,6 +91,16 @@ class EscolherPokemon
   end
 end
 
+#Classe de inicio do jogo
+
+class PokesoulsGame
+  def initialize
+    @pokebolas = 3
+    chance_fugir = 0.4
+    @pokemons_capturados = []
+    @pokemon_inicial = EscolherPokemon.escolherInicial
+  end
+end
 
 # Hash de Pokémons cadastrados para os bosses e para capturas
 
@@ -113,124 +122,154 @@ end
     19 => Pokemon.new("Mewtwo", 500, 150, "Psíquico",19)
   }
 
-def capturar_pokemon(pokemons_cadastrados, pokebolas, chance_fugir)
-  capturados = []
+ # Função de captura de Pokemons
 
-  while pokebolas > 0
-    puts "Você encontrou um Pokémon selvagem!"
-    puts "Pressione Enter para tentar capturá-lo..."
-    gets.chomp
+ def capturar_pokemon(pokemons_cadastrados, pokemons_capturados, pokebolas, chance_fugir)
+    system("clear") || system("cls")
 
-    if rand <= chance_fugir
-      puts "O Pokémon fugiu!"
-    else
-      id_pokemon = rand(5..19)
-      pokemon_aleatorio = pokemons_cadastrados.values.find { |pokemon|  }
+    while pokebolas > 0
+      puts "Você encontrou um Pokémon selvagem!"
+      puts "Pressione Enter para tentar capturá-lo..."
+      gets.chomp
 
-      if pokemon_aleatorio
-        capturados << pokemon_aleatorio
-        pokebolas -= 1
-        puts "Você capturou um #{pokemon_aleatorio.nome_pokemon}!"
-        puts "Você ainda tem #{pokebolas} Pokébolas restantes."
+      if rand <= chance_fugir
+        puts "O Pokémon fugiu!"
       else
-        puts "Nenhum Pokémon encontrado com o ID #{id_pokemon}."
+        id_pokemon = rand(5..19)
+        pokemon_aleatorio = pokemons_cadastrados.values.sample
+
+        if pokemon_aleatorio
+          pokemons_capturados << pokemon_aleatorio
+          pokebolas -= 1
+          puts "Você capturou um #{pokemon_aleatorio.nome_pokemon}!"
+          puts "Você ainda tem #{pokebolas} Pokébolas restantes."
+        else
+          puts "Nenhum Pokémon encontrado com o ID #{id_pokemon}."
+          system("clear") || system("cls")
+        end
       end
     end
+
+    puts "Você não tem mais Pokébolas!"
   end
 
-  puts "Você não tem mais Pokébolas!"
-  puts "Pokémons capturados:"
-  capturados.each do |pokemon|
-    puts "#{pokemon.nome_pokemon} (Tipo: #{pokemon.tipo}, HP: #{pokemon.hp})"
-  end
-end
 
+# Menu Do PokeSouls
+    loop do
+      puts "==========================="
+      puts "|  \e[31mƤօƙҽՏօuƖs\e[0m              |"
+      puts "==========================="
+      puts "|1. Progredir na história |"
+      puts "|2. Capturar Pokémon      |"
+      puts "|3. Ver história          |"
+      puts "|4. Meu time              |"
+      puts "|5. Sair                  |"
+      puts "==========================="
+      escolha = gets.chomp.to_i
 
-#Variaveis de captura de pokemons
-pokebolas = 5
-chance_fugir = 0.5
+    # Iicio do case de Menu
+      case escolha
+
+    #Modo História
+      when 1
+        puts "Progressão na história"
+
+    #Capturar Pokemons
+      when 2
+        capturar_pokemon(pokemons_cadastrados, pokemons_capturados, pokebolas, chance_fugir)
+
+    #História do Pokesouls
+    when 3
+
+      puts  "=========================================================================================="
+      puts  "História de ƤօƙҽՏօuƖs"
+      puts  "=========================================================================================="
+      puts "\n\e[35m(Ancião)\e[0m ~ \e[33m\e[3mNesses tempos antigos, a região de Kanto estava mergulhada em escuridão devido aos ataques dos Caçadores Rocket, servos leais do temível Rei Giovanni.\e[0m\e[0m\n"
+      print "\e[34m(Pressione Enter para Continuar a História)\e[0m\e[0m"
+      gets.chomp
+
+      puts "\n\e[35m (Ancião) \e[0m ~ \e[33m \e[3m Com cidades outrora pacíficas estavam sob constante ameaça, assoladas por pokémons corrompidos e monstros agressivos.\e[0m\e[0m\n"
+      print "\e[34m (Pressione Enter para Continuar a História) \e[0m"
+      gets.chomp
+
+      puts "\n\e[35m(Ancião)\e[0m ~ \e[33m\e[3mNo meio desse caos, um único treinador, munido de coragem e determinação, encontrou um parceiro improvável -\n um único Pokémon disposto a lutar por sua causa.\e[0m\e[0m\n"
+      print "\e[34m(Pressione Enter para Continuar a História)\e[0m"
+      gets.chomp
+
+      puts "\n\e[35m(Ancião)\e[0m ~ \e[33m\e[3mJuntos, eles começaram a desafiar os Caçadores Rocket, buscando unir outros treinadores e pokémons resistentes que compartilhavam sua vontade de desafiar o regime de Giovanni.\e[0m\n"
+      print "\e[34m(Pressione Enter para Continuar a História)\e[0m\n"
+      gets.chomp
+
+      puts "\e[35m(Ancião)\e[0m ~ \e[33m\e[3mAgora me entende #{nome}?\e[0m\e[0m\n"
+      print "\e[34m(Pressione Enter para Responder ao Ancião)\e[0m\n"
+      gets.chomp
+
+      puts  "\e[35m(#{nome})\e[0m ~ \e[33m\e[3mSim, Compreendo..... preciso fazer isso.......\e[33m\e[3m\e[0m\n"
+      puts  "==========================================================================================\n\n\n"
+    when 4
+      mostrar_pokemons_capturados(pokemon_inicial, pokemons_capturados)
+        puts "Pressione Enter para continuar..."
+        gets.chomp
+      when 5
+
+        puts "\e[35m(Ancião)\e[0m ~ \e[33m \e[3mChegou a hora de nos separarmos, mas apenas temporariamente. Que nossos caminhos se cruzem novamente em breve, e que vossas jornadas sejam seguras e prósperas. Até que nos encontremos novamente\e[0m \e[35m#{nome}.\e[0m\e[0m\n"
+        break
+      else
+
+        puts "Opção inválida! Escolha de 1 a 5."
+      end
+    end
+    end
+
 
 # começo do jogo
-puts "\e[35m Ancião ~ Em verdade vos digo, quem sois vós, senhor?\e[0m\n"
-print "Digite seu Nome nobre viajante:\n"
+
+puts "==========================="
+puts "|  \e[31mƤօƙҽՏօuƖs\e[0m              |"
+puts "==========================="
+
+
+
+puts "\e[35m (Ancião)\e[0m ~ \e[33m \e[3mEm verdade vos digo, quem sois vós, senhor?\e[0m\e[0m\n"
+print "\e[34m(Digite seu Nome nobre viajante:)\e[0m\n"
 nome = gets.chomp
 
-puts " \n\e[35mAncião\e[0m ~ \e[33mAh... meu bom #{nome}, compreenda que é uma tarefa árdua encontrar alguém nestas paragens.\n Desde o último ataque do Rei Giovanni, sobreviver tem se tornado uma empreitada difícil.\n Inúmeros embates contra os infames caçadores da Equipe Rocket e Pokémon agressivos têm assolado nossa jornada.\n Portanto, rogo que não vagueie desprotegido por essas terras.\n Venha, escolhamos um Pokémon!\e[0m\n\n"
+#Escolha o Pokemon
+
+puts " \n\e[35m(Ancião)\e[0m ~ \e[3m\e[33mAh... meu bom \e[35m#{nome}\e[0m\e[33m, \e[3mcompreenda que é uma tarefa árdua encontrar alguém nestas paragens.\n Desde o último ataque do Rei Giovanni, sobreviver tem se tornado uma empreitada difícil.\n Inúmeros embates contra os infames caçadores da Equipe Rocket e Pokémon agressivos têm assolado nossa jornada.\n Portanto, rogo que não vagueie desprotegido por essas terras.\n Venha, escolhamos um Pokémon!\e[0m\e[0m\n\n"
 
 pokemon_inicial = EscolherPokemon.escolherInicial
 puts "\e[32mVocê escolheu #{pokemon_inicial.checar_pokedex}!\e[0m"
 
-# Menu do PokeSouls
-loop do
-  puts "==========================="
-  puts "|  \e[31mƤօƙҽՏօuƖs\e[0m              |"
-  puts "==========================="
-  puts "|1. Progredir na história |"
-  puts "|2. Capturar Pokémon      |"
-  puts "|3. Ver história          |"
-  puts "|4. Meu time              |"
-  puts "|5. Sair                  |"
-  puts "==========================="
-  escolha = gets.chomp.to_i
+print "Começar"
 
-# Pókemos capturados
-capturados = []
 
-# Visualização de pokemons capturados
-def mostrar_pokemons_capturados(capturados)
-  if capturados.empty?
-    puts "Você ainda não capturou nenhum Pokémon."
-    puts "Somente está com o puts #{pokemon_inicial.nome_pokemon} (Tipo: #{pokemon.tipo}, HP: #{pokemon.hp} )"
-  else
+
+
+
+
+
+
+
+
+
+  pokemons_capturados = []
+
+  # Visualização de pokemons capturados
+  def mostrar_pokemons_capturados(pokemon_inicial, pokemons_capturados)
     puts "Pokémons capturados:"
-    capturados.each do |pokemon|
-      puts "#{pokemon_aleatorio.nome_pokemon} (Tipo: #{pokemon.tipo}, HP: #{pokemon.hp})"
-      puts "#{pokemon_inicial.nome_pokemon} (Tipo: #{pokemon.tipo}, HP: #{pokemon.hp} )"
+
+    if pokemons_capturados.empty?
+      puts "Você ainda não capturou nenhum Pokémon."
+    else
+      puts "Inicial: #{pokemon_inicial.nome_pokemon} (Tipo: #{pokemon_inicial.tipo}, HP: #{pokemon_inicial.hp})"
+
+      pokemons_capturados.each do |pokemon|
+        puts "#{pokemon.nome_pokemon} (Tipo: #{pokemon.tipo}, HP: #{pokemon.hp})"
+      end
     end
   end
-end
 
-
-# Iicio do case de Menu
-  case escolha
-  when 1
-    puts "Progressão na história"
-  when 2
-    capturar_pokemon(pokemons_cadastrados, pokebolas, chance_fugir)
-when 3
-  puts  "=========================================================================================="
-  puts  "História de ƤօƙҽՏօuƖs"
-  puts  "=========================================================================================="
-  puts "\n\e[35mAncião\e[0m ~ \e[33m\e[3mNesses tempos antigos, a região de Kanto estava mergulhada em escuridão devido aos ataques dos Caçadores Rocket, servos leais do temível Rei Giovanni.\e[0m\e[0m\n"
-  print "\e[34m(Pressione Enter para Continuar a História)\e[0m\e[0m"
-  gets.chomp
-
-  puts "\n\e[35mAncião\e[0m ~ \e[33m\e[3mCom cidades outrora pacíficas estavam sob constante ameaça, assoladas por pokémons corrompidos e monstros agressivos.\e[0m\e[0m\n"
-  print "\e[34m(Pressione Enter para Continuar a História)\e[0m"
-  gets.chomp
-
-  puts "\n\e[35mAncião\e[0m ~ \e[33m\e[3mNo meio desse caos, um único treinador, munido de coragem e determinação, encontrou um parceiro improvável -\n um único Pokémon disposto a lutar por sua causa.\e[0m\e[0m\n"
-  print "\e[34m(Pressione Enter para Continuar a História)\e[0m"
-  gets.chomp
-
-  puts "\n\e[35mAncião\e[0m ~ \e[33m\e[3mJuntos, eles começaram a desafiar os Caçadores Rocket, buscando unir outros treinadores e pokémons resistentes que compartilhavam sua vontade de desafiar o regime de Giovanni.\e[0m\n"
-  print "\e[34m(Pressione Enter para Continuar a História)\e[0m\n"
-  gets.chomp
-
-  puts "\e[35mAncião\e[0m ~ \e[33m\e[3mAgora me entende #{nome}?\e[0m\e[0m\n"
-  print "\e[34m(Pressione Enter para Responder ao Ancião)\e[0m\n"
-  gets.chomp
-
-  puts  "\e[35m#{nome}\e[0m ~ \e[33m\e[3mSim, Compreendo..... preciso fazer isso.......\e[33m\e[3m\e[0m\n"
-  puts  "==========================================================================================\n\n\n"
-
-when 4
-
-    puts "Pokémons capturados:"
-    mostrar_pokemons_capturados(capturados)
-  when 5
-    puts "\e[33mChegou a hora de nos separarmos, mas apenas temporariamente. Que nossos caminhos se cruzem novamente em breve, e que vossas jornadas sejam seguras e prósperas. Até que nos encontremos novamente,"  + nome
-  else
-    puts "Opção inválida! Escolha de 1 a 5."
-  end
-end
+# Inicialização do jogo
+game = PokesoulsGame.new
+game.run
