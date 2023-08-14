805e5c5d4c80765578d78062382c79878ba15bdf
Juan C <121655192+DevJuanzok4@users.noreply.github.com>
2023-08-14 23:18:47 +0000

    chore: adicionando requisições para as classes

---
+++ Pokesouls.rb
@@ -0,0 +1,5 @@
+# Requisições para as classes
+require_relative 'lib/personagens/player'
+require_relative 'lib/personagens/pokemon'
+require_relative 'lib/personagens/treinadores'
+require_relative 'lib/personagens//bosses'
