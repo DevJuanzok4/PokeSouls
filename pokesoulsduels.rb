7524151586ebb9a445a0df45fee1923c2bb0adb3
Juan C <121655192+DevJuanzok4@users.noreply.github.com>
2023-08-16 11:54:39 +0000

    chore: novas alterações

--- pokesoulsduels.rb
+++ pokesoulsduels.rb
@@ -47,6 +47,35 @@ class Movimento
   end
 end
 
+pokemons = [
+  Pokemon.new("Pikachu", "Elétrico", 100, 20, 5),
+  Pokemon.new("Bulbasaur", "Planta", 120, 18, 5),
+  Pokemon.new("Charmander", "Fogo", 110, 22, 5),
+  Pokemon.new("Greninja", "Água", 130, 25, 5),
+  Pokemon.new("Eevee", "Normal", 90, 15, 5),
+  Pokemon.new("Squirtle", "Água", 105, 19, 5),
+  Pokemon.new("Jigglypuff", "Fada", 95, 17, 5),
+  Pokemon.new("Machop", "Lutador", 115, 21, 5),
+  Pokemon.new("Geodude", "Pedra", 110, 20, 5),
+  Pokemon.new("Dratini", "Dragão", 100, 23, 5),
+  Pokemon.new("Gastly", "Fantasma", 90, 18, 5),
+  Pokemon.new("Meowth", "Normal", 95, 16, 5),
+  Pokemon.new("Koffing", "Venenoso", 100, 19, 5),
+  Pokemon.new("Ditto", "Normal", 80, 14, 5),
+  Pokemon.new("Onix", "Pedra", 120, 22, 5)
+]
+
+def Escolher_pokemon(pokemons,Player_1, Player_2)
+  puts "Escolha seu POkemon"
+  pokemons.each.with_index do |pokemon,index|
+    puts "#{index +1}. #{pokemon.nome}"
+end
+  print "#{.nome}, escolha um Pokémon (digite o número): "
+  escolha = gets.chomp.to_i
+
+  jogador.time_jogador << pokemons_disponiveis[escolha - 1]
+end
+
 puts "==========================="
 puts "|      ƤօƙҽՏօuƖs          |"
 puts "===========================\n\n"
